3cfee1030f6d0411db97f798a3099900659e4db0
SKY x LIVE <freefire948953@gmail.com>
2026-07-31 11:24:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ module.exports = {
       name: `NOTHING`,
       auth: `https://dsc.gg/ajidevserver`,
       secure: false
-    }
+    },
 
   // ─── Spotify API Credentials ───────────────────────────────────
   spotify: {
